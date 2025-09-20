8d2598781fb5f8db178f524fa2701dc49a511918
Hemanth kumar N <155843466+Tech-HemanthKumarN@users.noreply.github.com>
2025-09-20 17:03:22 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (7.62%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& arr) {
+        int n = arr.size();
+        int s = 0;
+        int e = n-1;
+        int mid = s + (e - s)/2;
+
+        while(s <= e){
+            //single element
+
+            if(s == e){
+                return arr[s];
+            }
+
+            //mid-1 index per value 
+            int leftValue = -1;
+            //mid+1 index per value
+            int rightValue = -1;
+            //mid index per value
+            int currValue = arr[mid];
+
+            if(mid - 1 >= 0){
+                leftValue = arr[mid - 1];
+            }
+            if(mid + 1 < n){
+                rightValue = arr[mid+1];
+            }
+
+            //case 2:
+            if(currValue != leftValue && currValue != rightValue ){
+                return currValue;
+            }
+            //case 3:
+            if(currValue == leftValue && currValue != rightValue){
+                int pairStartingIndex = mid - 1;  
+                if(pairStartingIndex & 1){
+                    e = mid -1;
+                }
+                else{
+                    s =mid+1;
+                }
+
+            }
+
+            //case 4:
+            else if( currValue == rightValue && currValue != leftValue){
+                int pairStartingIndex = mid;
+                if(pairStartingIndex & 1){
+                    e = mid -1;
+                }
+                else{
+                     s = mid +1;
+                }
+            }
+            mid = s + (e - s)/2;
+        }
+        return -1;
+        
+    }
+};
