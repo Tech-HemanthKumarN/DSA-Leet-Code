ae2d4a849591e345916f7ea8db85de2249da19e4
Hemanth kumar N <155843466+Tech-HemanthKumarN@users.noreply.github.com>
2025-09-20 21:13:06 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (91.96%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -3,59 +3,51 @@ public:
     int singleNonDuplicate(vector<int>& arr) {
         int n = arr.size();
         int s = 0;
-        int e = n-1;
-        int mid = s + (e - s)/2;
+        int e = n - 1;
+        int mid = s + (e - s) / 2;
 
-        while(s <= e){
-            //single element
-
-            if(s == e){
+        while (s <= e) {
+            if (s == e) {
+                // this is my single element
                 return arr[s];
             }
-
-            //mid-1 index per value 
+            // duplicate element does not exist
             int leftValue = -1;
-            //mid+1 index per value
             int rightValue = -1;
-            //mid index per value
             int currValue = arr[mid];
 
-            if(mid - 1 >= 0){
+            if (mid - 1 >= 0) {
                 leftValue = arr[mid - 1];
             }
-            if(mid + 1 < n){
-                rightValue = arr[mid+1];
+            if (mid + 1 < n) {
+                rightValue = arr[mid + 1];
             }
 
-            //case 2:
-            if(currValue != leftValue && currValue != rightValue ){
+            // case 1:
+            if (currValue != leftValue && currValue != rightValue) {
                 return currValue;
             }
-            //case 3:
-            if(currValue == leftValue && currValue != rightValue){
-                int pairStartingIndex = mid - 1;  
-                if(pairStartingIndex & 1){
-                    e = mid -1;
-                }
-                else{
-                    s =mid+1;
-                }
 
+            // case 2: duplicate exist in left side
+            if (currValue == leftValue && currValue != rightValue) {
+                int pairStartingIndex = mid - 1;
+                if (pairStartingIndex & 1) {
+                    e = mid - 1;
+                } else {
+                    s = mid + 1;
+                }
             }
-
-            //case 4:
-            else if( currValue == rightValue && currValue != leftValue){
+            // case 3: duplicate exist in right side
+            if (currValue == rightValue && currValue != leftValue) {
                 int pairStartingIndex = mid;
-                if(pairStartingIndex & 1){
-                    e = mid -1;
-                }
-                else{
-                     s = mid +1;
+                if (pairStartingIndex & 1) {
+                    e = mid - 1;
+                } else {
+                    s = mid + 1;
                 }
             }
-            mid = s + (e - s)/2;
+            mid = s + (e - s) / 2;
         }
         return -1;
-        
     }
 };
