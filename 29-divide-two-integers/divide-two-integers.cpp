b815d73cdb3308cd1cfbeb6ee568b4743400b587
Hemanth kumar N <155843466+Tech-HemanthKumarN@users.noreply.github.com>
2025-09-27 00:34:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (37.20%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
     int divide(int dividendx, int divisorx) {
-        if(dividendx == INT_MIN && divisorx == -1)
-        return INT_MAX;
+        if(dividendx == INT_MIN && divisorx == -1){
+            return INT_MAX;
+        }
 
         bool ans_pos_sign = true;
-        if(dividendx > 0 && divisorx< 0)
-        ans_pos_sign =  false;
-        if(dividendx < 0 && divisorx >0)
+        if(dividendx > 0 && divisorx < 0)
+        ans_pos_sign = false;
+        if(dividendx < 0 && divisorx > 0)
         ans_pos_sign = false;
 
-        long long int dividend = abs((long long)dividendx);
-        long long int divisor = abs((long long)divisorx);
-        long long  s = 0 , e = dividend;
-        long long  ans = 0;
+        long long  dividend = abs((long long )dividendx);
+        long long  divisor = abs((long long)divisorx);
+        long long s = 0, e = dividend;
+        long long ans = -1;
 
-        while( s<=e ){
+        while(s <= e){
             long long mid = s+((e-s)>>1);
-            if(divisor*mid <= dividend){
+            if( divisor * mid <= dividend){
                 ans = mid;
-                s = mid +1;
-            }else{
+                s = mid+1;
+            }
+            else{
                 e = mid - 1;
             }
         }
-    if(ans_pos_sign == false)
-    return -ans;
-    return ans;
+        if(ans_pos_sign == false)
+        return -ans;
+        return ans;
     }
-
 };
