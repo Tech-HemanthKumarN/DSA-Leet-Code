a3a0a5615de1a4298d123b488f34536a021cd69a
Hemanth kumar N <155843466+Tech-HemanthKumarN@users.noreply.github.com>
2025-09-27 17:13:21 +0530

    Time: 17 ms (14.05%) | Memory: 23 MB (11.89%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
-    bool canKokoFinishBananasInKSpeed(vector<int>& piles, int h, int k) {
+    bool canKokoFinishAllBananasInKSpeed(vector<int>&piles , int h, int k){
         long long int totalHoursTakenByKokoToFinishAllBananas = 0;
-        for (int i = 0; i < piles.size(); i++)
-            totalHoursTakenByKokoToFinishAllBananas +=
-                ceil(piles[i] / (double)k);
-        // if(totalHoursTakenByKokoToFinishAllBananas <= h){
-        //     return true;
-        // }
-        // else{
-        //     return  false;
-        // }
-        // or
-        return totalHoursTakenByKokoToFinishAllBananas <= h;
+        for(int i =0; i<piles.size(); i++){
+            totalHoursTakenByKokoToFinishAllBananas += ceil(piles[i] / (double)k);
+        }
+        return totalHoursTakenByKokoToFinishAllBananas <=h;
     }
+
     int minEatingSpeed(vector<int>& piles, int h) {
-        int start = 1;
-        int ans = -1;
-        // auto it = max_element(piles.begin(), piles.end());
-        // int end = *it;
-        int end = *max_element(piles.begin(), piles.end());
-        // end is pkka abs hai
+        int start  = 1;
+        int end = *max_element(piles.begin(),piles.end());
+        int ans  = -1;
 
-        while (start <= end) {
-            int mid = (start + end) >> 1;
+        while( start <= end){
+            int mid = (start + end)>>1;
             int k = mid;
-            
-            if (canKokoFinishBananasInKSpeed(piles, h, k)) {
-                // koko will finish all bananas
-                // without being caught
 
+            if(canKokoFinishAllBananasInKSpeed(piles,h,k)){
                 ans = k;
                 end = mid - 1;
-            } else
-            start = mid + 1;
+            }
+            else{
+                start = mid + 1;
+            }
         }
         return ans;
     }
