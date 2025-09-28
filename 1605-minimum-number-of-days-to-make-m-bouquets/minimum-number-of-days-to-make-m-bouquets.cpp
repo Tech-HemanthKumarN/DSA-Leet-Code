05a781e285cd2fb19308103d25c96af854d840ad
Hemanth kumar N <155843466+Tech-HemanthKumarN@users.noreply.github.com>
2025-09-28 21:05:19 +0530

    Time: 24 ms (44.40%) | Memory: 70.5 MB (76.13%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,15 +1,14 @@
 class Solution {
-public: 
-    bool canWeMakeMBouquetsWithDDays(vector<int>& bloomDay, int m, int k,int D){
+public:
+    bool canWeMakeMBouquetsWithDDays(vector<int>& bloomDay, int m, int k, int D){
         int counter = 0;
-        for(int i = 0; i<bloomDay.size(); i++){
+        for(int  i=0; i<bloomDay.size(); i++){
             if(bloomDay[i] <= D){
                 counter++;
             }
             if(counter == k){
                 m--;
                 counter = 0;
-
                 if(m == 0)
                 break;
             }
@@ -19,16 +18,14 @@ public:
         return m == 0;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
-        long long int Product = (long long )m * (long long)k;
-        if(bloomDay.size() < Product)
-        return -1;
-        int start = *min_element(bloomDay.begin(), bloomDay.end());
-        int end = *max_element(bloomDay.begin(), bloomDay.end());
+        int start = *min_element(bloomDay.begin(),bloomDay.end());
+        int end = *max_element(bloomDay.begin(),bloomDay.end());
         int ans = -1;
 
-        while (start <= end) {
-            int mid = (start + end) >> 1;
+        while(start <= end){
+            int mid = (start + end) >>1;
             int day = mid;
+
             if(canWeMakeMBouquetsWithDDays(bloomDay,m,k,day)){
                 ans = mid;
                 end = mid - 1;
@@ -38,5 +35,6 @@ public:
             }
         }
         return ans;
+        
     }
 };
