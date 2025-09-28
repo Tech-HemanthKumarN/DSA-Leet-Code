7665c9f9e6678255278e1e6159a168b0823b10dc
Hemanth kumar N <155843466+Tech-HemanthKumarN@users.noreply.github.com>
2025-09-28 17:51:58 +0530

    Time: 19 ms (81.43%) | Memory: 70.5 MB (76.13%) - LeetSync

---
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public: 
+    bool canWeMakeMBouquetsWithDDays(vector<int>& bloomDay, int m, int k, int D){
+        int counter = 0;
+        for(int i = 0; i <bloomDay.size(); i++){
+            //check whether the ith flower is bloomed or not
+            if(bloomDay[i] <= D){
+                // bloom ho gay hoga
+                counter++;
+            }
+            if(counter == k){
+                m--; //i can a bouqute
+                counter = 0;
+                if(m == 0)
+                break;
+            }
+            // not bloomed case
+            if(bloomDay[i]> D)
+            counter = 0 ;
+        }
+        return m ==0; //were u able to make M Bouqutes
+
+    }
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        int start = *min_element(bloomDay.begin(), bloomDay.end()); // at least one flower is bloomed
+        int end = *max_element(bloomDay.begin(), bloomDay.end()); // all flower is bloomed, pkka ban lung
+        int ans = -1;
+
+        while (start <= end) {
+            int mid = (start + end) >> 1;
+            int day = mid;
+
+            if (canWeMakeMBouquetsWithDDays(bloomDay, m, k, day)) {
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
