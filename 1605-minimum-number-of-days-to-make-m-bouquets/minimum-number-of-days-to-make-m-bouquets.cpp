8ad526bf3548ac63c7ba0db5f5a11a2b01acd297
Hemanth kumar N <155843466+Tech-HemanthKumarN@users.noreply.github.com>
2025-09-28 21:10:11 +0530

    Time: 24 ms (44.40%) | Memory: 70.6 MB (14.30%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,43 +1,45 @@
 class Solution {
-public:
+public: 
     bool canWeMakeMBouquetsWithDDays(vector<int>& bloomDay, int m, int k, int D){
         int counter = 0;
-        for(int  i=0; i<bloomDay.size(); i++){
+        for(int i = 0; i <bloomDay.size(); i++){
+            //check whether the ith flower is bloomed or not
             if(bloomDay[i] <= D){
+                // bloom ho gay hoga
                 counter++;
             }
             if(counter == k){
-                m--;
+                m--; //i  make can a bouqute
                 counter = 0;
                 if(m == 0)
                 break;
             }
-            if(bloomDay[i] > D)
-            counter = 0;
+            // not bloomed case
+            if(bloomDay[i]> D)
+            counter = 0 ;
         }
-        return m == 0;
+        return m ==0; //were u able to make M Bouqutes
+
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
-        double Product = (double)m *(double) k;
+        long long int Product = ((long long)m * (long long) k);
         if(bloomDay.size() < Product)
         return -1;
-        int start = *min_element(bloomDay.begin(),bloomDay.end());
-        int end = *max_element(bloomDay.begin(),bloomDay.end());
+        int start = *min_element(bloomDay.begin(), bloomDay.end()); // at least one flower is bloomed
+        int end = *max_element(bloomDay.begin(), bloomDay.end()); // all flower is bloomed, pkka ban lung
         int ans = -1;
 
-        while(start <= end){
-            int mid = (start + end) >>1;
+        while (start <= end) {
+            int mid = (start + end) >> 1;
             int day = mid;
 
-            if(canWeMakeMBouquetsWithDDays(bloomDay,m,k,day)){
+            if (canWeMakeMBouquetsWithDDays(bloomDay, m, k, day)) {
                 ans = mid;
                 end = mid - 1;
-            }
-            else{
+            } else {
                 start = mid + 1;
             }
         }
         return ans;
-        
     }
 };
