9c31249404b3d15e2089882a70663e0234044da3
Hemanth kumar N <155843466+Tech-HemanthKumarN@users.noreply.github.com>
2025-11-08 13:37:03 +0530

    Time: 3 ms (79.95%) | Memory: 17 MB (64.66%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
@@ -1,25 +1,57 @@
+// class Solution {
+// public:
+//     int findPairs(vector<int>& nums, int k) {
+// Method 1 Two Pointer approch
+// sort(nums.begin(),nums.end());
+// set<pair<int, int>> ans;
+
+// int i=0 , j=1;
+// while(j < nums.size()){
+//     int diff = nums[j] - nums[i];
+//     if(diff == k){
+//         ans.insert({nums[i], nums[j]});
+//         cout<<nums[i] <<" " << nums[j]<< endl;
+//         ++i, ++j;
+//     }
+//     else if(diff > k){
+//         i++;
+//     }
+//     else{
+//         j++;
+//     }
+//     if(i == j)
+//     j++;
+// }
+// return ans.size();
+
+//     }
+// };
+
+// Method 2
 class Solution {
 public:
+    int binarySearch(vector<int>& nums, int start, int x) {
+        int end = nums.size() - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (nums[mid] == x) {
+                return mid;
+            } else if (x > nums[mid]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
     int findPairs(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
+        sort(nums.begin(), nums.end());
         set<pair<int, int>> ans;
 
-        int i=0 , j=1;
-        while(j < nums.size()){
-            int diff = nums[j] - nums[i];
-            if(diff == k){
-                ans.insert({nums[i], nums[j]});
-                cout<<nums[i] <<" " << nums[j]<< endl;
-                ++i, ++j;
+        for (int i = 0; i < nums.size(); i++) {
+            if (binarySearch(nums, i + 1, nums[i] + k) != -1) {
+                ans.insert({nums[i], nums[i] + k});
             }
-            else if(diff > k){
-                i++;
-            }
-            else{
-                j++;
-            }
-            if(i == j)
-            j++;
         }
         return ans.size();
     }
