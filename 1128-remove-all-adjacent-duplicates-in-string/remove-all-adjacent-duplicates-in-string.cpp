9402f2c864ce03ecc1dbda6689335d6612955c52
Hemanth kumar N <155843466+Tech-HemanthKumarN@users.noreply.github.com>
2026-02-15 18:58:58 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (79.53%) - LeetSync

--- 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-        string removeDuplicates(string s) {
-        string ans = "";
-        int n = s.length();
+    string removeDuplicates(string s) {
+        // intialise ans string as empty string
+        string ans = "";
 
-        for (int i = 0; i < n; i++) {
-            char currChar = s[i];
+        int n = s.length();
 
-            if (ans.empty()) {
-                ans.push_back(currChar);
-            } 
-            else if (currChar == ans.back()) {
-                ans.pop_back();
-            } 
-            else if (currChar != ans.back()) {
-                ans.push_back(currChar);
-            }
-        }
-        return ans;
-           
-    }
+        for (int i = 0; i < n; i++) {
+            char currChar = s[i];
+            if (ans.empty()) {
+                // if ans is emty, directly push the currChar
+                ans.push_back(currChar);
+            } // right most charecter of ans = ans.back()
+            else if (currChar == ans.back()) {
+                ans.pop_back();
+            } else if (currChar != ans.back()) {
+                ans.push_back(currChar);
+            }
+        }
+
+        return ans;
+    }
 };
