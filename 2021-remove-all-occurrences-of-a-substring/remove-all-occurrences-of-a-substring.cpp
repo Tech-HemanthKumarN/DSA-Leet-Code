78922fb73d95c1e59a384880edf1124f1a7e9087
Hemanth kumar N <155843466+Tech-HemanthKumarN@users.noreply.github.com>
2026-02-16 23:02:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (70.56%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -2,9 +2,17 @@ class Solution {
 public:
     string removeOccurrences(string full, string pattern) {
 
-        while(full.find(pattern) != string::npos){
-            full.erase(full.find(pattern),pattern.length());
+        // In full string until the pattern string found run the loop
+        while (full.find(pattern) != string::npos) {
+            // erase in full string
+            //  in erase function we have 2 argumnet, in 1st argument we have
+            //  pattern starting index
+            // full.find(pattern), full string its providing the starting of the
+            // pattern in 2nd argument have to specify, how many char have to
+            // remove from starting index.
+            full.erase(full.find(pattern), pattern.length());
         }
+
         return full;
     }
 };
