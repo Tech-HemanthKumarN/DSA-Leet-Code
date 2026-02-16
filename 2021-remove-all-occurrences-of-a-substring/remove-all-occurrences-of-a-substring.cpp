9379b0c6fa192a31d0e9db7345471937f1854e33
Hemanth kumar N <155843466+Tech-HemanthKumarN@users.noreply.github.com>
2026-02-16 23:09:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (39.17%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,16 +1,39 @@
+// class Solution {
+// public:
+//     string removeOccurrences(string full, string pattern) {
+
+//         // In full string until the pattern string found run the loop
+//         while (full.find(pattern) != string::npos) {
+//             // erase in full string
+//             //  in erase function we have 2 argumnet, in 1st argument we have
+//             //  pattern starting index
+//             // full.find(pattern), full string its providing the starting of
+//             the
+//             // pattern in 2nd argument have to specify, how many char have to
+//             // remove from starting index.
+//             full.erase(full.find(pattern), pattern.length());
+//         }
+
+//         return full;
+//     }
+// };
+
 class Solution {
 public:
     string removeOccurrences(string full, string pattern) {
 
+        // find-- > vaild index return karega,  ya fer npos or - 1
+        int findKaIndex = full.find(pattern);
         // In full string until the pattern string found run the loop
-        while (full.find(pattern) != string::npos) {
+        while (findKaIndex != -1) {
             // erase in full string
             //  in erase function we have 2 argumnet, in 1st argument we have
             //  pattern starting index
             // full.find(pattern), full string its providing the starting of the
             // pattern in 2nd argument have to specify, how many char have to
             // remove from starting index.
-            full.erase(full.find(pattern), pattern.length());
+            full.erase(findKaIndex, pattern.length());
+            findKaIndex = full.find(pattern);
         }
 
         return full;
