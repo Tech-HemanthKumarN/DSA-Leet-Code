80b86eaec5667f61cc5ff7c183f63f18b8615870
Hemanth kumar N <155843466+Tech-HemanthKumarN@users.noreply.github.com>
2026-02-20 11:49:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (11.86%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -0,0 +1,20 @@
+static string orderCopy; // golbal variable means class variable
+
+class Solution {
+public:
+    static bool cmp(char& a, char& b) {
+        // login -> chek the order string which will come first and which will
+        // come next
+
+        // what will compartor operater will do means
+        // true --> a will be placed before b
+        // false --> b will be placed before a
+
+        return (orderCopy.find(a) < orderCopy.find(b));
+    }
+    string customSortString(string order, string s) {
+        orderCopy = order;
+        sort(s.begin(), s.end(), cmp);
+        return s;
+    }
+};
