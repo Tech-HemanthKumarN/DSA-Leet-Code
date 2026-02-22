5c0521e71e36133debee72b95232bf4aabb126f8
Hemanth kumar N <155843466+Tech-HemanthKumarN@users.noreply.github.com>
2026-02-22 11:12:24 +0530

    Time: 3 ms (68.08%) | Memory: 14.8 MB (52.00%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,15 +1,38 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& arr, int target) {
+//         int n = arr.size();
+
+//         for(int i = 0; i < n - 1; i++){
+//             for(int j = i + 1; j < n; j++){
+//                 if(arr[i] + arr[j] == target){
+//                     return {i,j};
+//                 }
+//             }
+//         }
+//         return {};
+//     }
+// };
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& arr, int target) {
-        int n =  arr.size();
-        for(int i=0; i<n-1; i++){
-            for(int j=i+1; j<n; j++){
-                if(arr[i]+arr[j] == target){
-                    return {i,j};
-                }
+
+        unordered_map<int,int> mapping;
+
+        vector<int> ans;
+
+        for (int i = 0; i < arr.size(); i++) {
+            int first = arr[i];
+            int second = target - first;
+
+            if (mapping.find(second) != mapping.end()) {
+                ans.push_back(i);
+                ans.push_back(mapping[second]);
+                break;
             }
+            mapping[first] = i;
         }
-        return {};
-        
+        return ans;
     }
 };
